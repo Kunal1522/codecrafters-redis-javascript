ac556ecf1daf1848d4770fa44a7b118e9bbd670f
Kunal1522 <kunalkashyap8024@gmail.com>
2025-10-14 19:53:27 +0000

    hi

--- app/main.js
+++ app/main.js
@@ -5,12 +5,20 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.on("data",(data) => {
-     const command=data.toString().split('\r\n');
-     if(command[2].toLowerCase()=="ping")
-        connection.write(`+PONG\r\n`);
-    else if(command[2].toLowerCase()=='echo')
-        connection.write(command[3]+'\r\n'+command[4]+'\r\n');
+
+  const redis_key_value_pair = new Map();
+  connection.on("data", (data) => {
+    const command = data.toString().split("\r\n");
+    if (command[2].toLowerCase() == "ping") connection.write(`+PONG\r\n`);
+    else if (command[2].toLowerCase() == "echo")
+      connection.write(command[3] + "\r\n" + command[4] + "\r\n");
+    else if (command[2].toLowerCase() == "set") {
+      redis_key_value_pair.set(command[4], command[6]);
+      connection.write(`+OK\r\n`);
+    } else if (command[2].toLowerCase() == "get") {
+      let value = redis_key_value_pair[command[4]];
+      connection.write(`$` + value.length() + `\r\n` + value + `\r\n`);
+    }
   });
 });
 
