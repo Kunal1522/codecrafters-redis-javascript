6ecb06a07756de09202e1f5b2a2572829d5f28ee
Kunal1522 <kunalkashyap8024@gmail.com>
2025-10-15 20:02:53 +0000

    codecrafters submit [skip ci]

--- app/handle_lrange.js
+++ app/handle_lrange.js
@@ -6,18 +6,22 @@ function lrange_handler(command,redis_list,connection) {
      start=redis_list[key].length+start;
     if(stop<0)
         stop=redis_list[key].length+stop;
-
+ start = Math.max(start, 0);
+ stop = Math.max(stop, 0);
+ start = Math.min(start, redis_list[key] ? redis_list[key].length - 1 : 0);
+ stop = Math.min(stop, redis_list[key] ? redis_list[key].length - 1 : 0);
   if (!redis_list[key]) {
     connection.write("*0\r\n");
     return;
   }
   const list = redis_list[key];
-
+const end = Math.min(stop, list.length - 1);
+ 
   if (start >= list.length || start > stop || stop < 0 || list.length == 0 || start < 0) {
     connection.write("*0\r\n");
     return;
   }
-  const end = Math.min(stop, list.length - 1);
+  
   connection.write(`*${end - start + 1}\r\n`);
   for (let i = start; i <= end; i++) {
     const val = list[i];
