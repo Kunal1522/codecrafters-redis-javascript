5426ec5eb097959b17e9d5879623d188592fb01a
Kunal1522 <kunalkashyap8024@gmail.com>
2025-10-21 21:52:39 +0000

    codecrafters submit [skip ci]

--- app/handlers/master_connector.js
+++ app/handlers/master_connector.js
@@ -13,7 +13,12 @@ function createMasterConnection() {
         );
       }, 100);
       setTimeout(() => {
-        connection.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
+        connection.write(
+          "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"
+        );
+      }, 100);
+      setTimeout(() => {
+        connection.write(`*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n`);
       }, 100);
     }
   );
