30ad68dcdf0fdd24d2d2708b501f947a3abe62a8
Kunal1522 <kunalkashyap8024@gmail.com>
2025-10-21 13:00:50 +0000

    codecrafters submit [skip ci]

--- app/config.js
+++ app/config.js
@@ -1,9 +1,12 @@
 let port = 6379;
 const args = process.argv;
-console.log(args);
+
 const portIndex = args.indexOf('--port');
-if (portIndex != -1 && args[portIndex + 1]) {
+if (portIndex !== -1 && args[portIndex + 1]) {
   port = parseInt(args[portIndex], 10);
 }
 console.log(port);
 export {port};
+
+
+
