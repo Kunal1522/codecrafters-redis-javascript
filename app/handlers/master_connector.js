07fbf2c400bec86117dde4476062ef32e8e85500
Kunal1522 <kunalkashyap8024@gmail.com>
2025-10-21 20:20:09 +0000

    codecrafters submit [skip ci]

--- app/handlers/master_connector.js
+++ app/handlers/master_connector.js
@@ -1,20 +1,26 @@
 import net from "net";
 import { serverConfig } from "../config.js";
-console.log("server configuration",serverConfig);
+console.log("server configuration", serverConfig);
 function createMasterConnection() {
   const connection = net.createConnection(
-    { port: serverConfig.master_port, host: "127.0.0.1"},
+    { port: serverConfig.master_port, host: "127.0.0.1" },
     () => {
       console.log("connected to master");
       connection.write(`*1\r\n$4\r\nPING\r\n`);
-      setTimeout(()=>{connection.write(`*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n${serverConfig.port}\r\n`)},100);
-      connection.write(()=>{`*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n`},100);
+      setTimeout(() => {
+        connection.write(
+          "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n"
+        );
+      }, 1000);
+      setTimeout(() => {
+        connection.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n");
+      }, 1000);
     }
   );
-  connection.on("data",(data)=>{
+  connection.on("data", (data) => {
     console.log(data);
-  })
+  });
   return connection;
 }
 
-export {createMasterConnection};
+export { createMasterConnection };
